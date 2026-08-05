56c08d76839633ae3e1e417d7201b18a8eb9f621
kenfrank7 <kendahlfrank20@gmail.com>
2026-08-05 00:33:48 -0500

    Update Hyperloop.py

--- Hyperloop.py
+++ Hyperloop.py
@@ -78,6 +78,7 @@ def get_asset_path(relative_path):
     """Get absolute path to resource relative to this script file"""
     base_path = os.path.dirname(os.path.realpath(__file__))
 
+    
     return os.path.normpath(os.path.join(base_path, relative_path))
 
 
